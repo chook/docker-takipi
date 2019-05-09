4d670a3fdf1800f9fc91eb417144830edd3fe731
Frank Chen <frank@intentmedia.com>
2019-05-09 10:42:36 -0400

    CK/FC: deploy docker to sa region, cleaned up the build code

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 image=intentmedia/takipi-remote
 cluster=ad-server
-region=us-east-1
+us_east_region=us-east-1
+sa_east_region=sa-east-1
 DOCKER_REGISTRY_ID=866983576462
 
 function login_to_ecr {
-	     eval `aws ecr get-login --registry-ids $DOCKER_REGISTRY_ID --no-include-email`
+	eval `aws ecr get-login --registry-ids $DOCKER_REGISTRY_ID --no-include-email`
 }
 
 function build {
@@ -14,29 +15,19 @@ function build {
 }
 
 function publish_container {
-      docker_registry="$DOCKER_REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com"
-	  docker tag $image:latest $docker_registry/$image:latest
-	  docker push $docker_registry/$image:latest
-}
-
-function build_and_publish_container {
-    login_to_ecr
-    build
-    publish_container
-}
-
-function tag_latest_container {
-    login_to_ecr
-    MANIFEST=$(aws ecr batch-get-image --repository-name $image --image-ids imageTag=$BUILD_NUMBER --query images[].imageManifest --output text)
-    aws ecr put-image --repository-name $image --image-tag latest --image-manifest "$MANIFEST"
+    docker_registry="$DOCKER_REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com"
+	docker tag $image:latest $docker_registry/$image:latest
+	docker push $docker_registry/$image:latest
 }
 
 function deploy_bid_optimization {
-    aws ecs update-service --cluster $cluster --region $region --service takipi --force-new-deployment
+    aws ecs update-service --cluster $cluster --region $us_east_region --service takipi --force-new-deployment
+    aws ecs update-service --cluster $cluster --region $sa_east_region --service takipi --force-new-deployment
 }
 
 function deploy {
-    build_and_publish_container
+    login_to_ecr
+    publish_container
     deploy_bid_optimization
 }
 
